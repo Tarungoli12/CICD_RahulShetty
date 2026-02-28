b62e8ee265d7f62b0541f523b7cdff21890c1881
Tarungoli12 <golisivasankartarun225@gmail.com>
2026-02-28 20:30:56 +0530

    2nd commit

--- src/test/java/com/automation/HomeLoan.java
+++ src/test/java/com/automation/HomeLoan.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class HomeLoan{
-    
+
     @Parameters({"URL"})
     @Test
     public void testCase3(String url){
